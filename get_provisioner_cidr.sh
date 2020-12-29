7a6c0acef2a9a260205af19a535effd3d9c5d3a5
Yan Low <me@yanlow.io>
2020-12-30 01:08:36 +0800

    Fixed cidr script

--- get_provisioner_cidr.sh
+++ get_provisioner_cidr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-CIDR=$(hostname -I)
+CIDR=$(dig +short myip.opendns.com @resolver1.opendns.com)
 CIDR=`echo $CIDR | sed 's/ *$//g'`
 CIDR="$CIDR/32"
 result='{"provisioner_cidr":"'"${CIDR}"'"}'
