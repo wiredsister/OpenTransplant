f59da974d42b0d343063ac378b4fa370658666e9
Yan <15334762+yanlow@users.noreply.github.com>
2021-01-17 03:22:11 +0800

    Updated docker-compose files; added redix

--- otapi/mix.exs
+++ otapi/mix.exs
@@ -41,7 +41,8 @@ defmodule Otapi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:redix, ">= 0.0.0"}
     ]
   end
 
