55c3cd55e12f9d38bc4dec225899e57c6c714713
Yan <15334762+yanlow@users.noreply.github.com>
2021-01-17 22:21:37 +0800

    Added tests

--- otapi/lib/otapi_web/controllers/organ_intake_request_controller.ex
+++ otapi/lib/otapi_web/controllers/organ_intake_request_controller.ex
@@ -1,15 +1,23 @@
-import Redix
-
 defmodule OtapiWeb.OrganIntakeRequestController do
   use OtapiWeb, :controller
 
   def create(conn, params) do
     case OtapiWeb.OrganIntakeRequestValidator.validate_organ_intake_request(params) do
       {:ok, organ_intake_request} ->
+        store_in_redis(organ_intake_request)
         conn |> send_resp(201, "Created")
       {:error, error} ->
         IO.puts(error)
         conn |> send_resp(400, "Bad Request")
+      _ ->
+        conn |> send_resp(500, "Internal Server Error")
+    end
+  end
+
+  defp store_in_redis(message) do
+    case Redix.command(:redix, ["LPUSH", "organ_intake_requests", Poison.encode!(message)]) do
+      {:ok, _} -> IO.puts("success")
+      {:error, e} -> IO.inspect(e)
     end
   end
 end
@@ -25,11 +33,8 @@ defmodule OtapiWeb.OrganIntakeRequestValidator do
       email: Map.get(params, "email")
     }
 
-    IO.inspect(Map.get(params, "blood_type"))
-    IO.inspect(Map.values(organ_intake_request))
-
-    if List.keyfind(Map.values(organ_intake_request), :error, 0) do
-      {:error, "bad input"}
+    if :error in Map.values(organ_intake_request) do
+      {:error, "validation error"}
     else
       {:ok, organ_intake_request}
     end
@@ -38,18 +43,18 @@ defmodule OtapiWeb.OrganIntakeRequestValidator do
   def get_blood_type(blood_type) do
     blood_types = ["A_Pos", "A_Neg", "B_Pos", "B_Neg", "AB_Pos", "AB_Neg", "O_Pos", "O_Neg"]
     if Enum.member?(blood_types, blood_type) do
-      {:ok, blood_type}
+      blood_type
     else
-      {:error, "bad blood_type"}
+      :error
     end
   end
 
   def get_body_size(body_size) do
     body_sizes = ["Infant", "Toddler", "Child", "Small_Female", "Small_Male", "Average_Female", "Average_Male", "Large_Female", "Large_Male"]
     if Enum.member?(body_sizes, body_size) do
-      {:ok, body_size}
+      body_size
     else
-      {:error, "bad body_size"}
+      :error
     end
   end
 
--- otapi/test/otapi_web/controllers/organ_intake_request_controller_test.exs
+++ otapi/test/otapi_web/controllers/organ_intake_request_controller_test.exs
@@ -14,6 +14,23 @@ defmodule OtapiWeb.OrganIntakeRequestControllerTest do
     assert conn.status == 201
   end
 
+  test "/organ-intake-request stores well-formed data in Redis" do
+    Redix.command!(:redix, ["DEL", "organ_intake_requests"])
+
+    json_body = %{
+      "blood_type" => "B_Pos",
+      "body_size" => "Average_Female",
+      "name" => "Tim Apple",
+      "email" => "tim@apple.com"
+    }
+
+    build_conn()
+      |> put_req_header("content-type", "application/json")
+      |> post("/api/organ-intake-requests", Poison.encode!(json_body))
+
+    assert Redix.command!(:redix, ["RPOP", "organ_intake_requests"]) |> Poison.decode! == json_body
+  end
+
   test "/organ-intake-request returns 400 for malformed data" do
     json_body = %{
       blood_type: "A_Pxs",
