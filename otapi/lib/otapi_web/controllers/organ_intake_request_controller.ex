33b3fa8de9a9a4384de10e8ecd5f377e658b4c4e
Yan <15334762+yanlow@users.noreply.github.com>
2021-01-18 22:52:46 +0800

    Refactored controller

--- otapi/lib/otapi_web/controllers/organ_intake_request_controller.ex
+++ otapi/lib/otapi_web/controllers/organ_intake_request_controller.ex
@@ -2,7 +2,7 @@ defmodule OtapiWeb.OrganIntakeRequestController do
   use OtapiWeb, :controller
 
   def create(conn, params) do
-    case OtapiWeb.OrganIntakeRequestValidator.validate_organ_intake_request(params) do
+    case OtapiWeb.OrganIntakeRequestValidatorService.validate_organ_intake_request(params) do
       {:ok, organ_intake_request} ->
         store_in_redis(organ_intake_request)
         conn |> send_resp(201, "Created")
@@ -20,42 +20,4 @@ defmodule OtapiWeb.OrganIntakeRequestController do
       {:error, e} -> IO.inspect(e)
     end
   end
-end
-
-defmodule OtapiWeb.OrganIntakeRequestValidator do
-
-  def validate_organ_intake_request(params) do
-
-    organ_intake_request = %{
-      blood_type: Map.get(params, "blood_type") |> OtapiWeb.OrganIntakeRequestValidator.get_blood_type,
-      body_size: Map.get(params, "body_size") |> OtapiWeb.OrganIntakeRequestValidator.get_body_size,
-      name: Map.get(params, "name"),
-      email: Map.get(params, "email")
-    }
-
-    if :error in Map.values(organ_intake_request) do
-      {:error, "validation error"}
-    else
-      {:ok, organ_intake_request}
-    end
-  end
-
-  def get_blood_type(blood_type) do
-    blood_types = ["A_Pos", "A_Neg", "B_Pos", "B_Neg", "AB_Pos", "AB_Neg", "O_Pos", "O_Neg"]
-    if Enum.member?(blood_types, blood_type) do
-      blood_type
-    else
-      :error
-    end
-  end
-
-  def get_body_size(body_size) do
-    body_sizes = ["Infant", "Toddler", "Child", "Small_Female", "Small_Male", "Average_Female", "Average_Male", "Large_Female", "Large_Male"]
-    if Enum.member?(body_sizes, body_size) do
-      body_size
-    else
-      :error
-    end
-  end
-
 end
